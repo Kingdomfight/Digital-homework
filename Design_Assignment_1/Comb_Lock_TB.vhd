c60352024ecbb4c6751c39855b906a01ca1798e1
Fer Clerkx <f.clerkx@student.fontys.nl>
2022-04-13 16:22:11 +0200

    Cleared up Design code, improved testbench

--- Design_Assignment_1/Comb_Lock_TB.vhd
+++ Design_Assignment_1/Comb_Lock_TB.vhd
@@ -7,28 +7,54 @@ end entity;
 
 architecture Simulation of Comb_Lock_TB is
 
-	constant c_CLKS_PER_DEBOUNCE : integer := 10;
+	constant c_CLK_PERIOD : time := 20 ns;				--50 MHz clock
+	constant c_CLKS_PER_DEBOUNCE : integer := 25E5;	--50ms debounce time
 	constant c_COMBINATION : integer := 1234;
-	constant c_CLK_PERIOD : time := 20 ns;
 	
-	constant c_1 : std_logic_vector(5 downto 0) := "100001";
-	constant c_2 : std_logic_vector(5 downto 0) := "010001";
-	constant c_3 : std_logic_vector(5 downto 0) := "001001";
-	constant c_4 : std_logic_vector(5 downto 0) := "100010";
-	constant c_5 : std_logic_vector(5 downto 0) := "010010";
-	constant c_6 : std_logic_vector(5 downto 0) := "001010";
-	constant c_7 : std_logic_vector(5 downto 0) := "100100";
-	constant c_8 : std_logic_vector(5 downto 0) := "010100";
-	constant c_9 : std_logic_vector(5 downto 0) := "001100";
-	
+	--Used to convert integer input to std_logic input
+	type t_Int_To_Key is array (1 to 9) of std_logic_vector(5 downto 0);
+	constant c_INT_TO_KEY : t_Int_To_Key := (1 => "100001",
+														  2 => "010001",
+														  3 => "001001",
+														  4 => "100010",
+														  5 => "010010",
+														  6 => "001010",
+														  7 => "100100",
+														  8 => "010100",
+														  9 => "001100");
+														  
 	signal r_CLK : std_logic := '0';
 	signal w_Keypad : std_logic_vector(5 downto 0);
 	signal w_Correct : std_logic;
 
+	--Simulates a bouncing button being pressed and unpressed
+	--Total sequence takes 160ms
+	procedure Press_Key(constant KEY : in integer;
+							  signal Keypad : out std_logic_vector(5 downto 0)) is
+	begin
+		Keypad <= c_INT_TO_KEY(KEY);
+		for i in 1 to 3 loop
+			wait for 5 ms;
+			Keypad <= (others => '0');
+			wait for 5 ms;
+			Keypad <= c_INT_TO_KEY(KEY);
+		end loop;
+		wait for c_CLKS_PER_DEBOUNCE * c_CLK_PERIOD * 2;
+		report "Pressed " & integer'image(KEY);
+		Keypad <= (others => '0');
+		for i in 1 to 3 loop
+			wait for 5 ms;
+			Keypad <= c_INT_TO_KEY(KEY);
+			wait for 5 ms;
+			Keypad <= (others => '0');
+		end loop;
+	end procedure Press_Key;
+	
 begin
 
 	r_CLK <= not r_CLK after c_CLK_PERIOD / 2;
 
+	--Instance of the code lock design
 	UUT_INST : entity work.Design_Assignment_1
 		generic map (
 			g_CLKS_PER_DEBOUNCE => c_CLKS_PER_DEBOUNCE,
@@ -42,20 +68,21 @@ begin
 		
 	process
 	begin
-		w_Keypad <= c_1;
-		report "Pressed 1";
-		wait for (c_CLKS_PER_DEBOUNCE + 2) * c_CLK_PERIOD;
-		w_Keypad <= c_2;
-		report "Pressed 2";
-		wait for (c_CLKS_PER_DEBOUNCE + 2) * c_CLK_PERIOD;
-		w_Keypad <= c_3;
-		report "Pressed 3";
-		wait for (c_CLKS_PER_DEBOUNCE + 2) * c_CLK_PERIOD;
-		w_Keypad <= c_4;
-		report "Pressed 4";
-		wait for (c_CLKS_PER_DEBOUNCE + 2) * c_CLK_PERIOD;
+		Press_Key(1, w_Keypad);
+		wait for 10 ms;
+		Press_Key(2, w_Keypad);
+		wait for 10 ms;
+		Press_Key(3, w_Keypad);
+		wait for 10 ms;
+		Press_Key(4, w_Keypad);
+		assert false severity failure;
+	end process;
+	
+	process
+	begin
 		wait until w_Correct = '1';
-		assert false report "Unlocked" severity failure;
+		report "Unlocked";
+		wait;
 	end process;
 
 end architecture Simulation;
--- Design_Assignment_1/Design_Assignment_1.vhd
+++ Design_Assignment_1/Design_Assignment_1.vhd
@@ -1,9 +1,28 @@
---i_Keypad bit 0: top row
---			  bit 1: middle row
---			  bit 2: bottom row
---			  bit 3: right column
---			  bit 4: middle column
---			  bit 5: left column
+--Design for code lock to be used with a 3x3 keypad. Debounce is implemented in the design.
+--The following keypad layout is assumed:			1	2	3
+--																4	5	6
+--																7	8	9
+--
+--g_CLKS_PER_DEBOUNCE: amount of clock cycles the signal needs to be stable high for an input to be registered
+--
+--g_COMBINATION: 4 digit combination that needs to be entered for the o_Correct signal to go high.
+--					  combination can't include any 0's.
+--
+--i_CLK: the input is sampled every rising edge of the clock
+--
+--i_Keypad: 6 bit input, one bit for every row and column
+--				when no button is depressed, all the input's should be logical low
+--				When a button is depressed, the corresponding row and column input should be logical high
+--				When more than one button is pressed, no input is registered
+--			 	bit 0: top row
+--			  	bit 1: middle row
+--			  	bit 2: bottom row
+--			  	bit 3: right column
+--			  	bit 4: middle column
+--			  	bit 5: left column
+--
+--o_Correct: '1' for one clock cycle after the correct combination has been put in
+
 
 library ieee;
 use ieee.std_logic_1164.all;
@@ -23,12 +42,14 @@ end entity Design_Assignment_1;
 
 architecture RTL of Design_Assignment_1 is
 
-	type t_Combination is array (3 downto 0) of integer range 0 to 9;
+	type t_Combination is array (3 downto 0) of integer range 0 to 9;		--0's are used when no input is button is pressed
 	type t_Counter is array (0 to 5) of integer range 0 to g_CLKS_PER_DEBOUNCE;
+	
 	constant c_CORRECT_COMB : t_Combination := (g_COMBINATION / 1000,
 															  (g_COMBINATION mod 1000) / 100,
 															  (g_COMBINATION mod 100) / 10,
 															  g_COMBINATION mod 10);
+															  
 	signal r_Prev_Input : std_logic_vector(5 downto 0) := (others => '0');
 	signal r_Counter : t_Counter := (others => g_CLKS_PER_DEBOUNCE);
 	signal r_Debounced_Input : std_logic_vector(5 downto 0) := (others => '0');
@@ -38,17 +59,19 @@ architecture RTL of Design_Assignment_1 is
 
 begin
 
+	--Determines debounced input state r_Debounced_Input
 	p_Debounce : process(i_CLK)
 	begin
 		if rising_edge(i_CLK) then
-			for i in 0 to 5 loop
+			for i in 0 to 5 loop		--Repeat logic for every input
 				r_Prev_Input(i) <= i_Keypad(i);
-				r_Debounced_Input(i) <= '0';
+				--Input has changed
 				if i_Keypad(i) /= r_Prev_Input(i) then
 					r_Counter(i) <= 0;
-					r_Counter(i) <= 0;
+				--Waiting for debounce delay
 				elsif r_Counter(i) < g_CLKS_PER_DEBOUNCE then
 					r_Counter(i) <= r_Counter(i) + 1;
+				--Input is debounced
 				else
 					r_Debounced_Input(i) <= i_Keypad(i);
 				end if;
@@ -56,9 +79,11 @@ begin
 		end if;
 	end process p_Debounce;
 	
+	--Determines which button is pressed down based on debounced input state
 	p_Keypad : process(r_Debounced_Input)
 	begin
 		case r_Debounced_Input(2 downto 0) is
+			--Top row
 			when "001" =>
 				case r_Debounced_Input(5 downto 3) is
 					when "001" => r_Pressed_Button <= 3;
@@ -66,6 +91,7 @@ begin
 					when "100" => r_Pressed_Button <= 1;
 					when others => r_Pressed_Button <= 0;
 				end case;
+			--Middle row
 			when "010" =>
 				case r_Debounced_Input(5 downto 3) is
 					when "001" => r_Pressed_Button <= 6;
@@ -73,6 +99,7 @@ begin
 					when "100" => r_Pressed_Button <= 4;
 					when others => r_Pressed_Button <= 0;
 				end case;
+			--Bottom row
 			when "100" =>
 				case r_Debounced_Input(5 downto 3) is
 					when "001" => r_Pressed_Button <= 9;
@@ -84,13 +111,16 @@ begin
 		end case;
 	end process p_Keypad;
 	
-	p_State_Machine : process(i_CLK)
+	--Keeps track of previous button presses and determines output correct signal
+	p_Shift_Register : process(i_CLK)
 	begin
 		if rising_edge(i_CLK) then
 			r_Prev_Pressed_Button <= r_Pressed_Button;
+			--Only track when button state goes from unpressed to depressed
 			if r_Pressed_Button /= 0 and r_Pressed_Button /= r_Prev_Pressed_Button then
-				r_User_Input <= r_User_Input(2 downto 0) & r_Pressed_Button;
+				r_User_Input <= r_User_Input(2 downto 0) & r_Pressed_Button;	--Shift left
 			end if;
+			--Check if correct combination has been entered
 			if r_User_Input = c_CORRECT_COMB then
 				o_Correct <= '1';
 				r_User_Input <= (others => 0);
@@ -98,5 +128,5 @@ begin
 				o_Correct <= '0';
 			end if;
 		end if;
-	end process p_State_Machine;
+	end process p_Shift_Register;
 end architecture RTL;
